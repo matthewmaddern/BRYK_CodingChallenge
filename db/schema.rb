cf65186b3f90ab6616873c631fde314fdd0831f6
Matt Maddern <matt_mdn@icloud.com>
2022-02-22 21:17:35 +1030

    add Users scaffold

---
+++ db/schema.rb
@@ -0,0 +1,31 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_02_22_103428) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "goods", force: :cascade do |t|
+    t.string "cosignment_id"
+    t.string "type"
+    t.string "name"
+    t.string "source"
+    t.string "destination"
+    t.time "entry_time"
+    t.time "exit_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cosignment_id"], name: "index_goods_on_cosignment_id", unique: true
+  end
+
+end
