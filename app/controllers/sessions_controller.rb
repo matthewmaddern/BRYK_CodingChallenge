3cd2d42438325e8b66748f3abb45b3f1ef5bf7f6
Matt Maddern <matt_mdn@icloud.com>
2022-02-23 19:05:42 +1030

    add basic login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,14 @@
 class SessionsController < ApplicationController
+  # POST /login
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      flash[:notice] = 'Incorrect username or password.'
+      redirect_to '/login'
+    end
+  end
 end
