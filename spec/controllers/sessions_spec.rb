04938ffc8a7a8904853173cfb4781a3fc9f0c8f7
Matt Maddern <matt_mdn@icloud.com>
2022-02-23 20:28:04 +1030

    add first RSpec test case to test authentication

---
+++ spec/controllers/sessions_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe GoodsController, type: :controller do
+  fixtures :users
+
+  describe "GET /index" do
+
+    it "will edirect unauthorised users" do
+      get :index
+
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(login_path)
+    end
+
+    it "will accept authorised users" do
+      get :index, session: { user_id: 1 }
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
