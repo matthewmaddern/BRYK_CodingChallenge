cc856803b8eb61043ebd05f7621c03614423f4a6
Matt Maddern <matt_mdn@icloud.com>
2022-02-24 15:46:17 +1030

    add Goods validations

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,2 +1,8 @@
 class Good < ApplicationRecord
+  validates :cosignment_id, presence: true, uniqueness: true
+  validates :good_type, presence: true
+  validates :name, presence: true
+  validates :source, presence: true
+  validates :destination, presence: true
+  validates :entry_time, presence: true
 end
