695302ef40e445a6111602ad580610e2d3af27b1
Matt Maddern <matt_mdn@icloud.com>
2022-02-24 16:50:52 +1030

    fix documentation in test case

--- spec/controllers/goods_spec.rb
+++ spec/controllers/goods_spec.rb
@@ -6,17 +6,11 @@ RSpec.describe GoodsController, type: :controller do
   fixtures :users
 
   describe "POST /bulk-upload" do
-    it "will redirect unauthorised users" do
+    it "will accept a csv file to be uploaded" do
       upload_file = Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/files/bulk_upload.csv")
       post :bulk_upload, params: { file: upload_file }, session: { user_id: 1 }
 
       expect(response).to have_http_status(:no_content)
-    end
-
-    it "will accept authorised users" do
-      get :index, session: { user_id: 1 }
-
-      expect(response).to have_http_status(:ok)
     end
   end
 end
